3393c2aeff99b8e397439f6106337385dad3b2d0
Damian Stasiak <poczta@damianstasiak.pl>
2022-11-27 14:39:26 +0700

    continue working on menu

--- ex_01_connection_to_db.py
+++ ex_01_connection_to_db.py
@@ -96,31 +96,36 @@ def add_task(conn,task):
 def just_hello():
     print("Just hello")
 
-create_projects_sql = """
-    -- projects table
-    CREATE TABLE IF NOT EXISTS projects (
-        id integer PRIMARY KEY,
-        nazwa text NOT NULL,
-        start_date text,
-        end_date text
-    );
-    """
+def sql_fetch(con):
+    cursorObj = con.cursor()
+    cursorObj.execute('SELECT name from sqlite_master where type= "table"')
+    print(cursorObj.fetchall())
 
-if __name__ == '__main__':
-   
-    create_tasks_sql = """
-    -- zadanie table
-    CREATE TABLE IF NOT EXISTS tasks (
-        id integer PRIMARY KEY,
-        projekt_id integer NOT NULL,
-        nazwa VARCHAR(250) NOT NULL,
-        opis TEXT,
-        status VARCHAR(15) NOT NULL,
-        start_date text NOT NULL,
-        end_date text NOT NULL,
-        FOREIGN KEY (projekt_id) REFERENCES projects (id)
-    );
-    """
+create_projects_sql = """
+-- projects table
+CREATE TABLE IF NOT EXISTS projects (
+    id integer PRIMARY KEY,
+    nazwa text NOT NULL,
+    start_date text,
+    end_date text
+);
+"""
+
+create_tasks_sql = """
+-- zadanie table
+CREATE TABLE IF NOT EXISTS tasks (
+    id integer PRIMARY KEY,
+    projekt_id integer NOT NULL,
+    nazwa VARCHAR(250) NOT NULL,
+    opis TEXT,
+    status VARCHAR(15) NOT NULL,
+    start_date text NOT NULL,
+    end_date text NOT NULL,
+    FOREIGN KEY (projekt_id) REFERENCES projects (id)
+);
+"""
+
+if __name__ == '__main__':  
 
     now1 = datetime.now()
     time1 = now1.strftime("%H:%M:%S")
--- menu.py
+++ menu.py
@@ -7,7 +7,9 @@ import ex_01_connection_to_db
 def menu():
     print("--------MENU--------------------------")
     print("1 - Create table projects")
-    print("2 - ")
+    print("2 - Create table tasks")
+    print("3 - show tables")
+    print("4 - dodaj project")
     print("0 - exit")
     print("--------------------------------------")
     print("")
@@ -18,15 +20,18 @@ def main_loop(conn):
         menu()
         input1 = input()
         if (input1 == '1'):
+            print("Dodaje tabele projects")
             ex_01_connection_to_db.just_hello()
             ex_01_connection_to_db.execute_sql(conn,ex_01_connection_to_db.create_projects_sql)
-
         elif (input1 == '2'):
-            print("Wybrales 2")
+            print("Dodaje tabele tasks")
+            ex_01_connection_to_db.just_hello()
+            ex_01_connection_to_db.execute_sql(conn,ex_01_connection_to_db.create_tasks_sql)
         elif (input1 == '3'):
-            print("Wybrales 3")
+            print("Lista tabel w bazie danych")
+            ex_01_connection_to_db.sql_fetch(conn)
         elif (input1 == '4'):
-            print("Wybrales 4")
+            print("Dodawanie projektu")
         elif (input1 == '5'):
             print("Wybrales 5")
         elif (input1 == '6'):
